8bcf4a660b437eb2ee154be03e0cf39b0c2fc3b2
lzj4822948 <douglas4822948@gmail.com>
2015-12-01 17:33:29 +0800

    no message

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public float moveSpeed = 1f;
+	public float rotateSpeed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(new Vector3(0, 0, 1) * moveSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate(new Vector3(0, 0, -1) * moveSpeed * Time.deltaTime);		
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate(new Vector3(0, -1, 0) * rotateSpeed * Time.deltaTime);		
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(new Vector3(0, 1, 0) * rotateSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot: MonoBehaviour {
+	public GameObject shoot;
+	public Transform obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown () {
+		if (Input.GetKey (KeyCode.Space)) {
+			shoots();
+		}
+	}
+
+	void shoots (){
+		Instantiate(shoot, obj.position, obj.rotation);
+	}
+}
